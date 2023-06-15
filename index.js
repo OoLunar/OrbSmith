49e32f0e43402f36840cddacdc51a84fe7084411
Lunar Starstrum <lunar@forsaken-borders.net>
2023-06-15 16:33:18 -0500

    Added docs

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+// Spotify Player Configuration
 const clientId = "dbfc2c5b7bfd450892824b70eb136a47";
 const redirectUri = window.location.href.split('?')[0];
+
+// DOM elements
 const body = document.getElementsByTagName("body")[0];
 const loading = document.getElementById("loading");
 const loginButton = document.getElementById("login-button");
@@ -10,25 +13,35 @@ const songTitle = document.getElementById("song-title");
 const songArtist = document.getElementById("song-artist");
 const songAlbum = document.getElementById("song-album");
 
-// Generate a secure random string using the browser crypto API
+/**
+ * Generates a secure random string using the browser crypto API.
+ * @returns {string} A random string.
+ */
 function generateRandomString() {
     const array = new Uint32Array(28);
     window.crypto.getRandomValues(array);
     return Array.from(array, dec => ('0' + dec.toString(16)).substr(-2)).join('');
 }
 
-// Calculate the SHA256 hash of the input text.
-// Returns a promise that resolves to an ArrayBuffer
+/**
+ * Calculates the SHA256 hash of the input text.
+ * @param {string} plain - The input text.
+ * @returns {Promise<ArrayBuffer>} A promise that resolves to an ArrayBuffer.
+ */
 function sha256(plain) {
     const encoder = new TextEncoder();
     const data = encoder.encode(plain);
     return window.crypto.subtle.digest("SHA-256", data);
 }
 
-// Base64-urlencodes the input string
+/**
+ * Base64-urlencodes the input string.
+ * @param {string} str - The input string.
+ * @returns {string} The base64-url encoded string.
+ */
 function base64UrlEncode(str) {
     // Convert the ArrayBuffer to string using Uint8 array to convert to what btoa accepts.
-    // btoa accepts chars only within ascii 0-255 and base64 encodes
+    // btoa accepts chars only within ascii 0-255 and base64 encodes.
     // This line takes base64 strings and converts them to base64url strings.
     // See here for the reason why we need to make this replacement:
     // https://developer.spotify.com/documentation/general/guides/authorization-guide/#implicit-grant-flow
@@ -36,7 +49,11 @@ function base64UrlEncode(str) {
     return base64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
 }
 
-// Handle errors and display appropriate messages
+/**
+ * Handles errors and displays appropriate messages.
+ * @param {string} message - The error message.
+ * @param {boolean} [clearStorage=false] - Whether to clear the local storage.
+ */
 function handleError(message, clearStorage = false) {
     if(clearStorage) {
         localStorage.clear();
@@ -49,6 +66,9 @@ function handleError(message, clearStorage = false) {
     console.error(message);
 }
 
+/**
+ * Initiates the login process.
+ */
 async function login() {
     // Generate the base64-urlencoded sha256 hash for the PKCE challenge
     const codeVerifier = generateRandomString();
@@ -68,6 +88,10 @@ async function login() {
     window.location.href = `https://accounts.spotify.com/authorize?${params}`;
 }
 
+/**
+ * Initializes the Spotify player.
+ * @param {string} token - The access token.
+ */
 function startApp(token) {
     const player = new Spotify.Player({
         name: "OBS Spotify Player",
@@ -89,6 +113,10 @@ function startApp(token) {
     player.connect();
 }
 
+/**
+ * Updates the song information displayed on the app.
+ * @param {object} state - The player state object.
+ */
 function updateSongInfo(state) {
     if(state) {
         const { current_track: currentTrack } = state.track_window;
@@ -124,8 +152,12 @@ function updateSongInfo(state) {
         pauseIcon.style.display = "none";
         body.style.opacity = 1; // keep app at full opacity when not playing
     }
-};
+}
 
+/**
+ * Initiates playing after receiving the authorization code.
+ * @param {string} code - The authorization code.
+ */
 function startPlaying(code) {
     // We have an authorization code, now we need to exchange it for an access token
     const codeVerifier = localStorage.getItem("pkce_code_verifier");
